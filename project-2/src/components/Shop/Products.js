22fb4b8c2ba4225b70c563949469ab8f56846328
marovichn <nikolamarovic.11@gmail.com>
2023-04-10 18:03:25 +0200

    finished-project-2.0.2

--- project-2/src/components/Shop/Products.js
+++ project-2/src/components/Shop/Products.js
@@ -2,22 +2,22 @@ import { useSelector } from "react-redux";
 import ProductItem from "./ProductItem";
 import classes from "./Products.module.css";
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "id1",
+    title: "Book",
+    price: 6,
+    description: "This is a first product - amazing!",
+  },
+  {
+    id: "id2",
+    title: "Ball",
+    price: 7,
+    description: "This is a second product - amazing!",
+  },
+];
 const Products = (props) => {
-  const products = [
-    {
-      id: "id1",
-      title: "Test",
-      price: 6,
-      description: "This is a first product - amazing!",
-    },
-    {
-      id: "id2",
-      title: "Test2",
-      price: 7,
-      description: "This is a second product - amazing!",
-    },
-  ];
-  const productsContent = products.map((item) => {
+  const productsContent = DUMMY_PRODUCTS.map((item) => {
     return <ProductItem key={item.id} title={item.title} item={item} />;
   });
   return (
