4b9ff138b203eaca11e4275cbdc732218bfcf705
marovichn <nikolamarovic.11@gmail.com>
2023-04-11 19:23:27 +0200

    one-async-solution

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -2,9 +2,11 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.showCart);
+  const cart = useSelector((state) => state.cart);
 
   return (
     <Layout>
