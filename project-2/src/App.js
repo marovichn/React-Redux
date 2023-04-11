52afe631b980c3784a72590f65aa86877e1b4dc9
marovichn <nikolamarovic.11@gmail.com>
2023-04-11 19:51:36 +0200

    error-handling-1

--- project-2/src/App.js
+++ project-2/src/App.js
@@ -1,24 +1,48 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
+import { uiActions } from "./store/uiSlice";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.showCart);
   const cart = useSelector((state) => state.cart);
 
   useEffect(() => {
-    fetch(
-      "https://simple-react-app-2b7b6-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      {
-        method: "PUT",
-        body: JSON.stringify(cart),
-        headers: {
-          "Content-Type": "app/json",
-        },
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending data...",
+          message: "Cart data is sending...",
+        })
+      );
+      const response = await fetch(
+        "https://simple-react-app-2b7b6-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+          headers: {
+            "Content-Type": "app/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
       }
-    );
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Successfully sent cart data :)",
+        })
+      );
+    };
+
+    sendCartData().catch();
   }, [cart]);
 
   return (
