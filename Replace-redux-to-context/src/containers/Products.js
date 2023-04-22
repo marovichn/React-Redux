3b6d0f9426e6717a9a96ac90075a8e547a9432ea
marovichn <nikolamarovic.11@gmail.com>
2023-04-22 19:40:57 +0200

    custom-store-hoook

--- Replace-redux-to-context/src/containers/Products.js
+++ Replace-redux-to-context/src/containers/Products.js
@@ -1,13 +1,12 @@
 import React, { useContext } from "react";
-import { useSelector } from "react-redux";
-
-import { ProductsContext } from "../context/products-context";
 
 import ProductItem from "../components/Products/ProductItem";
 import "./Products.css";
+import { useStore } from "../hooks-store/store";
 
 const Products = () => {
-  const productList = useContext(ProductsContext).products;
+  const [state, dispatch] = useStore();
+  const productList = state.products;
   return (
     <ul className="products-list">
       {productList.map((prod) => (
